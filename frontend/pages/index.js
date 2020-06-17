62c45f8011b51ebd6b1a01a5e569b6ea868e6f5e
Tom Hill <tomhill98@me.com>
2020-06-17 10:10:49 +1000

    adds head element example

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,12 +4,20 @@ import Hello from "../components/Hello";
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="columns is-centered">
-        <div className="box column is-6">
-          <h1 className="title">Welcome to PantryPirate</h1>
-          <p>Probs good idea to read README.md</p>
-          <Hello />
+    <div>
+      <Head>
+        <title>Pantry Pirate</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <div className="container">
+        <div className="columns is-centered">
+          <div className="box column is-6">
+            <h1 className="title">Welcome to PantryPirate</h1>
+            <p>Probs good idea to read README.md</p>
+            <Hello />
+          </div>
         </div>
       </div>
     </div>
