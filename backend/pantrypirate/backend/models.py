d61dfa246b9cce7f5700900a23f993a74c74605a
RohanDugdale <rohandugdale@gmail.com>
2020-06-21 22:39:49 +1000

    Testing Django, made draft models

--- backend/pantrypirate/backend/models.py
+++ backend/pantrypirate/backend/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+import datetime
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=60)
+    password = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class DietaryRequirement(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class MealCategory(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    cook_time = models.TimeField()
+    method = models.CharField(max_length=5000)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="author_recipe")
+    favourite = models.ManyToManyField(User, related_name="favourite_recipe")
+    diet_req = models.ManyToManyField(DietaryRequirement)
+    meal_cat = models.ManyToManyField(MealCategory)
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientCategory(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+    category = models.ForeignKey(IngredientCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    adjective = models.CharField(max_length=30)
+    unit = models.CharField(max_length=20)
+    amount = models.CharField(max_length=5)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ingredient.name
+
+
+class PantryIngredient(models.Model):
+    expiry_date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ingredient.name
+
--- backend/pantrypirate/backend/tests.py
+++ backend/pantrypirate/backend/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import *
+
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(name="Bob",
+                                         email="Potato17@gmail.com",
+                                         password="4321")
+
+    def test_print(self):
+        assert(print(self.user) == "Bob")
+
+    def test_deets(self):
+        assert(self.user.name =="Bob")
+        assert(self.user.email == "Potato17@gmail.com")
+        assert(self.user.password == "4321")
+
