c3644507db4fd314c5063d2456ca4a2da3e00c35
Tom Hill <tomhill98@me.com>
2020-06-25 17:12:57 +1000

    adds links to nav

--- frontend/components/nav/Nav.js
+++ frontend/components/nav/Nav.js
@@ -1,26 +1,23 @@
-import styles from './Nav.module.scss'
-import Brand from '../icons/Brand'
-import Explore from '../icons/Explore'
-import Cookbook from '../icons/Cookbook'
-import Create from '../icons/Create'
-import Settings from '../icons/Settings'
-import Pantry from '../icons/Pantry'
-import NavItem from './NavItem'
+import styles from "./Nav.module.scss";
+import Brand from "../icons/Brand";
+import Explore from "../icons/Explore";
+import Cookbook from "../icons/Cookbook";
+import Create from "../icons/Create";
+import Settings from "../icons/Settings";
+import Pantry from "../icons/Pantry";
+import NavItem from "./NavItem";
 
 export default function Nav() {
-
-    return <nav className={styles.nav}>
-
-        <ul>
-            <NavItem icon={<Brand/>} brand={true}/>
-            <NavItem icon={<Explore/>} name={'Explore'}/>
-            <NavItem icon={<Pantry/>} name={'Pantry'}/>
-            <NavItem icon={<Cookbook/>} name={'Cookbook'}/>
-            <NavItem icon={<Create/>} name={'Create'}/>
-            <NavItem icon={<Settings/>} name={'Settings'}/>
-        </ul>
-
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <NavItem icon={<Brand />} brand={true} href="/" />
+        <NavItem icon={<Explore />} name={"Explore"} href="/explore" />
+        <NavItem icon={<Pantry />} name={"Pantry"} href="/" />
+        <NavItem icon={<Cookbook />} name={"Cookbook"} href="/cookbook" />
+        <NavItem icon={<Create />} name={"Create"} href="/recipe/create" />
+        <NavItem icon={<Settings />} name={"Settings"} href="/settings" />
+      </ul>
     </nav>
-        
-    
+  );
 }
--- frontend/components/nav/NavItem.js
+++ frontend/components/nav/NavItem.js
@@ -1,13 +1,18 @@
-import styles from './NavItem.module.scss'
+import styles from "./NavItem.module.scss";
+
+import Link from "next/link";
 
 export default function NavItem(props) {
-    return <li className={'brand' in props ? styles.brand : styles.box}>
+  return (
+    <Link href={props.href}>
+      <li className={"brand" in props ? styles.brand : styles.box}>
         {props.icon}
-        {'brand' in props ? '' : 
-        <span className={styles.tag}>
-            {props.name}
-        </span>
-        }
-        
-    </li>
+        {"brand" in props ? (
+          ""
+        ) : (
+          <span className={styles.tag}>{props.name}</span>
+        )}
+      </li>
+    </Link>
+  );
 }
