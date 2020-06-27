2accda4576cdef36f226dcac651716e4b60f9494
Tom Hill <tomhill98@me.com>
2020-06-27 22:05:11 +1000

    fixes annoying className error

--- frontend/components/modal/Modal.js
+++ frontend/components/modal/Modal.js
@@ -1,17 +1,18 @@
-import styles from "./Modal.module.scss"
-
+import styles from "./Modal.module.scss";
 
 export default function Login(props) {
-    function toggle() {
-        document.getElementById(props.id).classList.toggle("is-active");
-    }
-    return <div id={props.id} className="modal">
-        <div className="modal-background" onClick={toggle}/>
-        <div className={`modal-content ${styles.box}`}>
-            {props.content}
-
-        </div>
-        <button class="modal-close is-large" aria-label="close" onClick={toggle}></button>
+  function toggle() {
+    document.getElementById(props.id).classList.toggle("is-active");
+  }
+  return (
+    <div id={props.id} className="modal">
+      <div className="modal-background" onClick={toggle} />
+      <div className={`modal-content ${styles.box}`}>{props.content}</div>
+      <button
+        className="modal-close is-large"
+        aria-label="close"
+        onClick={toggle}
+      ></button>
     </div>
-
+  );
 }
