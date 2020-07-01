faef28d1ee3c7f9c2fe4dd2798cdfc537df1de08
Tom Hill <tomhill98@me.com>
2020-07-02 09:46:25 +1000

    Update recipe page

--- frontend/pages/recipe/[recipeId].js
+++ frontend/pages/recipe/[recipeId].js
@@ -12,12 +12,41 @@ function Recipe(props) {
       </Head>
       <div className="container">
         <div className="columns is-centered">
-          <div className="box column is-6">
-            <h1 className="title">{props.name}</h1>
+          <div className="box column is-10">
+            <h1 className="title is-2">{props.name}</h1>
+            <img src="https://source.unsplash.com/1200x600/?food" />
             <p>
               Author: {props.author.username} | Cook time: {props.cook_time}
             </p>
-            <p>{props.method}</p>
+            <div className="tags">
+              {props.diet_req.map((diet, idx) => (
+                <span className="tag" key={idx}>
+                  {diet}
+                </span>
+              ))}
+            </div>
+            <div className="buttons">
+              <a className="button is-light">Edit</a>
+              <a className="button is-light is-danger">Delete</a>
+            </div>
+            <hr />
+            <div className="columns">
+              <div className="column is-4">
+                <h4 className="title is-4">Ingredients</h4>
+                <ul>
+                  {props.ingredients.map((ingredient, idx) => (
+                    <li key={idx}>
+                      {ingredient.amount} {ingredient.unit}{" "}
+                      {ingredient.ingredient}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="column is-6">
+                <h4 className="title is-4">Method</h4>
+                <p>{props.method}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
