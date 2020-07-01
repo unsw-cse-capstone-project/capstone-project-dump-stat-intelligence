ef89d035f1f970b67b3416ac63738a20bc7cae91
Tom Hill <tomhill98@me.com>
2020-07-01 18:21:51 +1000

    permission changes

--- backend/pantrypirate/backend/views.py
+++ backend/pantrypirate/backend/views.py
@@ -1,34 +1,46 @@
 from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import (
+    TokenAuthentication,
+    SessionAuthentication,
+    BasicAuthentication,
+)
+from rest_framework.permissions import IsAuthenticated
 from .serializers import *
 from .models import *
 from rest_framework.views import Response, Http404
 import json
 
+
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('-id')
+    queryset = User.objects.all().order_by("-id")
     serializer_class = UserSerializer
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    queryset = Recipe.objects.all().order_by('name')
+    queryset = Recipe.objects.all().order_by("name")
     serializer_class = RecipeSerializer
 
 
 class IngredientViewSet(viewsets.ModelViewSet):
-    queryset = Ingredient.objects.all().order_by('name')
+    queryset = Ingredient.objects.all().order_by("name")
     serializer_class = IngredientSerializer
 
 
 class PantryIngredientViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication]
-    queryset = PantryIngredient.objects.all().order_by('ingredient')
+    authentication_classes = [
+        TokenAuthentication,
+        SessionAuthentication,
+        BasicAuthentication,
+    ]
+    permission_classes = [IsAuthenticated]
+    queryset = PantryIngredient.objects.all().order_by("ingredient")
     serializer_class = PantryIngredientSerializer
 
     def list(self, request, *args, **kwargs):
         if request.user:
-            queryset = PantryIngredient.objects.filter(
-                user=request.user).order_by('ingredient')
+            queryset = PantryIngredient.objects.filter(user=request.user).order_by(
+                "ingredient"
+            )
             serializer = self.get_serializer(queryset, many=True)
             return Response(json.dumps(serializer.data))
         else:
