21dd5bf60c89fd8e4c1b33e094566509a12b7cb0
Tom Hill <tomhill98@me.com>
2020-07-02 09:50:10 +1000

    Attempt to fix CORS errors

--- backend/pantrypirate/pantrypirate/settings.py
+++ backend/pantrypirate/pantrypirate/settings.py
@@ -30,9 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS' :
-        'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE' : 10
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 
@@ -44,10 +43,13 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "backend.apps.BackendConfig",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
