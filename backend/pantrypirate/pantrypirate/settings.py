a7276864e34386ce1ba76c8c96f5a060b622deb6
Tom Hill <tomhill98@me.com>
2020-07-02 12:31:22 +1000

    fuck me CORS is the biggest pile of bullshit i've ever seen

--- backend/pantrypirate/pantrypirate/settings.py
+++ backend/pantrypirate/pantrypirate/settings.py
@@ -43,21 +43,21 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "corsheaders",
     "backend.apps.BackendConfig",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 
 ROOT_URLCONF = "pantrypirate.urls"
 
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = ("http://localhost:3000",)
+
--- frontend/pages/recipe/[recipeId].js
+++ frontend/pages/recipe/[recipeId].js
@@ -1,63 +1,105 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
+import { withRouter } from "next/router";
+import React from "react";
 
 import RecipeAPI from "../../lib/api/recipe";
+import Error from "../../components/Error/Error";
 
-function Recipe(props) {
-  console.log(props);
-  return (
-    <div>
-      <Head>
-        <title>Pantry Pirate | Create</title>
-      </Head>
-      <div className="container">
-        <div className="columns is-centered">
-          <div className="box column is-10">
-            <h1 className="title is-2">{props.name}</h1>
-            <img src="https://source.unsplash.com/1200x600/?food" />
-            <p>
-              Author: {props.author.username} | Cook time: {props.cook_time}
-            </p>
-            <div className="tags">
-              {props.diet_req.map((diet, idx) => (
-                <span className="tag" key={idx}>
-                  {diet}
-                </span>
-              ))}
-            </div>
-            <div className="buttons">
-              <a className="button is-light">Edit</a>
-              <a className="button is-light is-danger">Delete</a>
-            </div>
-            <hr />
-            <div className="columns">
-              <div className="column is-4">
-                <h4 className="title is-4">Ingredients</h4>
-                <ul>
-                  {props.ingredients.map((ingredient, idx) => (
-                    <li key={idx}>
-                      {ingredient.amount} {ingredient.unit}{" "}
-                      {ingredient.ingredient}
-                    </li>
-                  ))}
-                </ul>
+class Recipe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      recipe: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    RecipeAPI.get(this.props.router.query.recipeId).then(
+      ({ data }) => {
+        console.log(data);
+        this.setState({ recipe: data, loading: false });
+      },
+      (err) => {
+        console.log(err);
+        this.setState({ error: err.response, loading: false });
+      }
+    );
+  }
+
+  handleDelete = async (e) => {
+    this.setState({ loading: true });
+    const res = await RecipeAPI.delete(this.state.recipe.id, ""); // TODO: token authentication
+    console.log(res);
+    this.setState({ loading: false });
+  };
+
+  render() {
+    if (this.state.error) {
+      return <Error message={this.state.error.statusText} />;
+    }
+
+    if (this.state.loading) {
+      return <p>Loading</p>;
+    }
+    return (
+      <div>
+        <Head>
+          <title>Pantry Pirate | Create</title>
+        </Head>
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="box column is-10">
+              <h1 className="title is-2">{this.state.recipe.name}</h1>
+              <img
+                src={`https://source.unsplash.com/1200x600/?${this.state.recipe.name}`}
+              />
+              <p>
+                Author: {this.state.recipe.author.username} | Cook time:{" "}
+                {this.state.recipe.cook_time}
+              </p>
+              <div className="tags">
+                {this.state.recipe.diet_req.map((diet, idx) => (
+                  <span className="tag" key={idx}>
+                    {diet}
+                  </span>
+                ))}
               </div>
-              <div className="column is-6">
-                <h4 className="title is-4">Method</h4>
-                <p>{props.method}</p>
+              <div className="buttons">
+                <a className="button is-light">Edit</a>
+                <a
+                  className="button is-light is-danger"
+                  onClick={this.handleDelete}
+                >
+                  Delete
+                </a>
+              </div>
+              <hr />
+              <div className="columns">
+                <div className="column is-4">
+                  <h4 className="title is-4">Ingredients</h4>
+                  <ul>
+                    {this.state.recipe.ingredients.map((ingredient, idx) => (
+                      <li key={idx}>
+                        {ingredient.amount} {ingredient.unit}{" "}
+                        {ingredient.ingredient.name}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="column is-6">
+                  <h4 className="title is-4">Method</h4>
+                  <p>{this.state.recipe.method}</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-Recipe.getInitialProps = async ({ query: { recipeId } }) => {
-  const { data } = await RecipeAPI.get(recipeId);
-
-  return data;
-};
-
-export default Recipe;
+// You can't use hooks (i.e. useRouter) with a class component in react
+export default withRouter(Recipe);
