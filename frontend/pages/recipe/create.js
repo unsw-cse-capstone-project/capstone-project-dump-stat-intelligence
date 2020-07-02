b45ea50252c4ec4bec474f989789cd27b15d8bc2
Tom Hill <tomhill98@me.com>
2020-07-02 14:47:26 +1000

    new recipe template

--- frontend/pages/recipe/create.js
+++ frontend/pages/recipe/create.js
@@ -1,22 +1,78 @@
 import Head from "next/head";
+import { withRouter } from "next/router";
+import React from "react";
 
-import Hello from "../../components/Hello";
+import RecipeAPI from "../../lib/api/recipe";
+import Error from "../../components/Error/Error";
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Pantry Pirate | Create</title>
-      </Head>
-      <div className="container">
-        <div className="columns is-centered">
-          <div className="box column is-6">
-            <h1 className="title">/recipe/create</h1>
-            <p>Probs good idea to read README.md</p>
-            <Hello />
+class Recipe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      recipe: {
+        title: "",
+        method: "",
+        cook_time: "",
+        author: "",
+      },
+      error: null,
+    };
+  }
+
+  componentDidMount() {}
+
+  handleDelete = async (e) => {};
+
+  render() {
+    if (this.state.error) {
+      return <Error message={this.state.error.statusText} />;
+    }
+
+    if (this.state.loading) {
+      return <p>Loading</p>;
+    }
+    return (
+      <>
+        <Head>
+          <title>Pantry Pirate | Create</title>
+        </Head>
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="box column is-10">
+              <h1 className="title">New Recipe</h1>
+              <form>
+                <div className="field">
+                  <label className="label">Title</label>
+                  <input className="input" placeholder="Title" />
+                </div>
+                <div className="field">
+                  <label className="label">Cook Time</label>
+                  <input className="input" placeholder="Cook Time" />
+                </div>
+                <div className="field">
+                  <label className="label">Method</label>
+                  <textarea
+                    className="textarea"
+                    placeholder="Method"
+                  ></textarea>
+                </div>
+              </form>
+              <br />
+
+              <div className="buttons">
+                <button className="button is-light is-success">
+                  Add Recipe
+                </button>
+                <button className="button is-light is-danger">Discard</button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
+      </>
+    );
+  }
 }
+
+// You can't use hooks (i.e. useRouter) with a class component in react
+export default withRouter(Recipe);
