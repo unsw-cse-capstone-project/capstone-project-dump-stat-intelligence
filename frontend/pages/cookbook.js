c1405352ff73f6c9733bcc331ffc316378ff5d1c
zachjbrowning <zachjbrowning@outlook.com>
2020-07-07 16:01:54 +1000

    working on cookbook

--- frontend/pages/cookbook.js
+++ frontend/pages/cookbook.js
@@ -1,9 +1,27 @@
-import HighLevel from "../components/cookbook/HighLevel";
+import Head from "next/head";
+
+import { useSelector } from 'react-redux';
+import RecipeIcon from "../components/cookbook/RecipeIcon";
 
 export default function Home() {
+  const favourites = useSelector(state => state.recipes.recipes)
+  console.log(favourites)
   return (
     <>
-      <HighLevel/>
+      <Head>
+        <title>Pantry Pirate | Cookbook</title>
+      </Head>
+      <div>
+        <br/>
+        <h1 className="title is-3">My Favourited recipes</h1>
+        <div className="columns is-multiline">
+          {favourites.map((recipe, idx) => (
+            <div key={idx} className="column is-3">
+              <RecipeIcon title={recipe.name} id={recipe.id}/>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
