20b4ee804360d916892cd604f2a48f0ba411923f
zachjbrowning <zachjbrowning@outlook.com>
2020-07-11 18:59:27 +1000

    started ingredient editing

--- frontend/components/creator/Ingredient.js
+++ frontend/components/creator/Ingredient.js
@@ -5,7 +5,7 @@ import styles from "./Edit.module.scss";
 export default function Ingredient(props) {
 
     return <>
-        <div>
+        <div key={props.idx}>
             {`${props.ingredient.amount} ${props.ingredient.adjective} ${props.ingredient.ingredient}s`}
         </div>
 
--- frontend/components/creator/IngredientEdit.js
+++ frontend/components/creator/IngredientEdit.js
@@ -1,7 +1,17 @@
 import styles from "./Edit.module.scss";
 
-
+import { useSelector, useDispatch } from "react-redux";
+import Ingredient from "./Ingredient";
 
 export default function IngredientEdit() {
-    
+    let ingredients = useSelector(state => state.create.ingredients);
+    return <div>
+        <div className="tags">
+            {ingredients.map((item, idx) => (
+                <Ingredient key={idx} idx={idx} ingredient={item}/>
+            ))}
+        </div>
+
+
+    </div>
 }
