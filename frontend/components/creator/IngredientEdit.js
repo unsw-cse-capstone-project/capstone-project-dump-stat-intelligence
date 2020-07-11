fe81cc0bc005283ac5f04d87b4e3dfc69f6a5a6c
zachjbrowning <zachjbrowning@outlook.com>
2020-07-11 19:37:21 +1000

    work in progress

--- frontend/components/creator/IngredientEdit.js
+++ frontend/components/creator/IngredientEdit.js
@@ -2,16 +2,43 @@ import styles from "./Edit.module.scss";
 
 import { useSelector, useDispatch } from "react-redux";
 import Ingredient from "./Ingredient";
+import IngredientSearch from "../Pantry/IngredientSearch";
+
 
 export default function IngredientEdit() {
     let ingredients = useSelector(state => state.create.ingredients);
+    let searchId="searcher"
     return <div>
         <div className="tags">
             {ingredients.map((item, idx) => (
                 <Ingredient key={idx} idx={idx} ingredient={item}/>
             ))}
         </div>
-
-
+        
+        <h1 className="title is-4">Add ingredients</h1>
+        <div className={`control ${styles.querySearch}`}>
+            <input id={searchId} className="input" placeholder="Add an item"/>
+        </div>
+        <form onSubmit={(event) => {event.preventDefault(); dispatch(explore_add(event.target.elements.choice.value));}} autocomplete="false">
+            <div className="control">
+                <label className="label">Choose an ingredient to cook with</label>
+                <div className={`field is-grouped ${styles.formCon}`}>
+                    <div className="control">
+                        <input className="input"/>
+                    </div>
+                    
+                    <div className="control">
+                        <button type="submit" className="button">
+                            Add
+                        </button>
+                    </div>
+                    <div className="control">
+                        <button onClick={(event) => basic_dispatch(event, explore_clear)} className="button">
+                        Clear
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 }
