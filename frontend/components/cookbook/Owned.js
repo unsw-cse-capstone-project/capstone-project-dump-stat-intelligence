681ac3872ed3802a60f05753f96a92a0bacb63d4
Tom Hill <tomhill98@me.com>
2020-07-30 20:31:56 +1000

    rewording of titles

--- frontend/components/cookbook/Owned.js
+++ frontend/components/cookbook/Owned.js
@@ -1,37 +1,45 @@
 import styles from "./Cookbook.module.scss";
 
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import RecipeIcon from "./RecipeIcon";
 import Arrow from "./Arrow";
 
 export default function Favourites() {
-    const owned = useSelector((state) => state.auth.owned)
-    function toggleIt(id) {
-        document.getElementById(`${id}-icon`).classList.toggle(styles.arrowUp);
-        let box = document.getElementById(`${id}-box`)
-        if (box.style.maxHeight) {
-          box.style.maxHeight = box.scrollHeight + 'px';
-          setTimeout(() => {box.style.maxHeight = null}, 305);
-        } else {
-          box.style.maxHeight = box.scrollHeight + 'px';
-          setTimeout(() => box.style.maxHeight = '0px', 5);
-        }
-      }
+  const owned = useSelector((state) => state.auth.owned);
+  function toggleIt(id) {
+    document.getElementById(`${id}-icon`).classList.toggle(styles.arrowUp);
+    let box = document.getElementById(`${id}-box`);
+    if (box.style.maxHeight) {
+      box.style.maxHeight = box.scrollHeight + "px";
+      setTimeout(() => {
+        box.style.maxHeight = null;
+      }, 305);
+    } else {
+      box.style.maxHeight = box.scrollHeight + "px";
+      setTimeout(() => (box.style.maxHeight = "0px"), 5);
+    }
+  }
 
-
-    return <>
-        <div onClick={() => toggleIt("owned")} className={styles.header}>
-            <h1 className="title is-3">My Owned recipes</h1>
-            <Arrow name="owned-icon"/>
-        </div> 
-        <div id="owned-box" className={`${styles.drawer} columns is-multiline`}>
-            <div className={styles.buffer}></div>
-            {owned.map((recipe, idx) => (
-                <div key={idx} className="column is-3">
-                <RecipeIcon owned={true} fave={true} title={recipe.name} id={recipe.id} src={recipe.image_URL}/>
-                </div>
-            ))}
-        </div>
+  return (
+    <>
+      <div onClick={() => toggleIt("owned")} className={styles.header}>
+        <h1 className="title is-3">My Recipes</h1>
+        <Arrow name="owned-icon" />
+      </div>
+      <div id="owned-box" className={`${styles.drawer} columns is-multiline`}>
+        <div className={styles.buffer}></div>
+        {owned.map((recipe, idx) => (
+          <div key={idx} className="column is-3">
+            <RecipeIcon
+              owned={true}
+              fave={true}
+              title={recipe.name}
+              id={recipe.id}
+              src={recipe.image_URL}
+            />
+          </div>
+        ))}
+      </div>
     </>
-    
+  );
 }
