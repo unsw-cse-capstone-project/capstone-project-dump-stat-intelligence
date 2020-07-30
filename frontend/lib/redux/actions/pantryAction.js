27e4269543fb6b0ee4534fd26e5696e7ce45513a
Tom Hill <tomhill98@me.com>
2020-07-30 18:08:41 +1000

    tom fixes his mistakes

--- frontend/lib/redux/actions/pantryAction.js
+++ frontend/lib/redux/actions/pantryAction.js
@@ -17,36 +17,40 @@ PANTRY
 //NO API, frontend only
 export const change = (ingredient, category, expiry) => async (dispatch) => {
   dispatch({
-    type : types.PANTRY_CHANGE,
-    ingredient : ingredient, 
-    category : category,
-    expiry : expiry,
-  })
-}
+    type: types.PANTRY_CHANGE,
+    ingredient: ingredient,
+    category: category,
+    expiry: expiry,
+  });
+};
 
 export const add = (ingredient) => async (dispatch) => {
   let auth = store.getState().auth;
   let response;
-  let exists = false
+  let exists = false;
   let i;
 
   if (auth.isLoggedIn) {
     response = await PantryAPI.get(10, 1, "");
 
     for (i = 0; i < response.data.length; i++) {
-      if (response.data[i]['ingredient']['name'] == ingredient.name) {
-        exists = true
+      if (response.data[i]["ingredient"]["name"] == ingredient.name) {
+        exists = true;
       }
     }
   }
 
   if (exists == false) {
-    await PantryAPI.add({'expiry_date': ingredient.expiry, 'user': auth.uid, 'ingredient': ingredient.name});
+    await PantryAPI.add({
+      expiry_date: ingredient.expiry,
+      user: auth.uid,
+      ingredient: ingredient.name,
+    });
 
     let newIngredient = {
       category: ingredient.category,
       name: ingredient.name,
-      expiry : ingredient.expiry
+      expiry: ingredient.expiry,
     };
 
     dispatch({
@@ -66,10 +70,9 @@ export const remove = (ingredient) => async (dispatch) => {
     response = await PantryAPI.get(10, 1, "");
 
     for (i = 0; i < response.data.length; i++) {
-
-      if (response.data[i]['ingredient']['name'] == ingredient.ingredient) {
-        console.log(response.data[i]['id'])
-        await PantryAPI.delete(response.data[i]['id']);
+      if (response.data[i]["ingredient"]["name"] == ingredient.ingredient) {
+        console.log(response.data[i]["id"]);
+        await PantryAPI.delete(response.data[i]["id"]);
       }
     }
   }
@@ -77,5 +80,24 @@ export const remove = (ingredient) => async (dispatch) => {
     category: ingredient.category,
     ingredient: ingredient.ingredient,
   };
-  let auth = store.getState().auth;
   dispatch({
+    type: types.PANTRY_REMOVE,
+    toRemove: toRemove,
+  });
+};
+
+export const get_pantry = () => async (dispatch) => {
+  let auth = store.getState().auth;
+  let newPantry = {};
+
+  if (auth.isLoggedIn) {
+    response = await PantryAPI.get(10, 1, ""); // TODO: token auth, error handling
+    newPantry = response.data;
+  }
+
+  dispatch({
+    type: types.PANTRY_GET,
+    pantry: newPantry,
+    T,
+  });
+};
